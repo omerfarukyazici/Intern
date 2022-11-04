f5d5512145028e02f61fa3be2da125838251eb5e
omer faruk yazici <omeryazici6175@gmail.com>
2022-11-04 19:12:46 +0300

    first commit

---
+++ src/main/java/com/internmain/InternApplication.java
@@ -0,0 +1,36 @@
+package com.internmain;
+
+import com.internmain.models.Student;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class InternApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternApplication.class, args);
+	}
+	@GetMapping
+	public List<Student> hello(){
+		return List.of(
+				new Student(
+						1L,
+						"Maria",
+						"mariam@gmail.com",
+						LocalDate.of(2000, Month.JANUARY,5),
+						22
+
+				)
+		);
+	}
+	}
+
+
---
+++ src/main/java/com/internmain/resources/HomeResources.java
@@ -0,0 +1,17 @@
+/*package com.internmain.resources;
+
+import com.internmain.config.SpringBootConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResources {
+    @Autowired
+    SpringBootConfiguration springBootConfiguration;
+    @GetMapping("/")
+    public String merhaba(){
+        return springBootConfiguration.company;
+    }
+}
+*/
